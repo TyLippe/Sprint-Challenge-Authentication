c4776fc6d7f7be5141e45ffe02ba2a8ed36b969b
Ty Lippe <tylippe94@gmail.com>
2019-07-26 12:19:34 -0400

    added login

--- config/routes.js
+++ config/routes.js
@@ -26,7 +26,26 @@ function register(req, res) {
 }
 
 function login(req, res) {
-  // implement user login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+          res.status(200).json({
+            message: `Welcom ${user.username}!`,
+            token
+          });
+      } else {
+        res.status(401).json({
+          message: 'Invalid Credentials'
+        })
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
 }
 
 function getJokes(req, res) {
